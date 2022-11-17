ba292884922cdf1259da9db63002ea5cdc617b56
Ekaterina Vinnikova <red-white.katya@mail.ru>
2022-11-17 18:34:40 +0300

    2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+
+import { Command } from 'commander/esm.mjs';
+const program = new Command();
+program
+  .option('-h, --help', 'display help for command')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-v, --vers', 'output the number version');
+
+  program.parse();
+  
