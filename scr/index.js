7d3f9f1abdc5173a7153cdcf254baec44daf5226
Ekaterina Vinnikova <red-white.katya@mail.ru>
2022-11-23 15:25:07 +0300

    s6

--- scr/index.js
+++ scr/index.js
@@ -9,17 +9,13 @@ const genDiff = (file1, file2) => {
         const keys = Object.keys({ ...string1, ...string2 });
         const sortKeys = _.sortBy(keys);
     const resultKey = sortKeys.map((key) => {
-        if (!_.has(file1, key)) {
-            return { key, value2: file2[key], type: 'add' }
-    }
-    if ((!_.has(file2, key))) {
-        return { key, value1: file1[key], type: 'delet' }
-    }
-    if (file1[key] !== file2[key]) {
-        return { key, value1: file1[key], value2: file2[key], type: 'chang' }
-        }
-    return { key, value1: file1[key] };
-    });
+        const minus = '-';
+        const plus = '+';
+        if (Object.hasOwn(file1, key) & Object.hasOwn(file2, key)) {
+            return { minus, kye: file1[key], plus, key: file2[key]}
+        };
+        
+        
     return resultKey;
 }
 };
