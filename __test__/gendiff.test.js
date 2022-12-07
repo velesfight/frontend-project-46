1cc5a27a87c7adda2b0a82bed808f9085b0dd981
Ekaterina Vinnikova <red-white.katya@mail.ru>
2022-12-07 18:07:29 +0300

    test

---
+++ __test__/gendiff.test.js
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import genDiff from '../scr/index.js';
+import path from 'path';
+import fs from 'fs';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const file1json = getFixturePath('file1.json');
+const file2json = getFixturePath('file2.json');
+const actual1 = genDiff(file1json, file2json);
+const expectedResul = readFile('result.txt');
+test('gendiff.json', () => {
+    expect(actual1).toEqual(expectedResul);
+  });
