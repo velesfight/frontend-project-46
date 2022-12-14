ec76d9df40a4be1e14944bac257d151337d3c733
Ekaterina Vinnikova <red-white.katya@mail.ru>
2022-12-14 12:51:08 +0300

    tests

--- scr/diffFunc.js
+++ scr/diffFunc.js
@@ -4,7 +4,7 @@ const genDiff1 = (file1, file2) => {
   const keys = Object.keys({ ...file1, ...file2 });
   const sortKeys = _.sortBy(keys);
   const result = [];
-  for (const key of sortKeys) {
+  sortKeys.forEach((key) => {
     const value1 = file1[key];
     const value2 = file2[key];
     if (!_.has(file1, key)) {
@@ -14,9 +14,9 @@ const genDiff1 = (file1, file2) => {
     } else if (value2 !== value1) {
       result.push(`- ${key}: ${value1}\n+ ${key}: ${value2}`);
     } else {
-     result.push(`  ${key}: ${value1}`);
+      result.push(`  ${key}: ${value1}`);
     }
-  }
   return `{\n${result.join('\n')}\n}`;
-};
+});
+}
 export default genDiff1;
